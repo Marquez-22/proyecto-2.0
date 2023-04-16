fa6b82150add81b2b6059b8151907849089b9eba
fatima <marquezlinares19@gmail.com>
2023-04-16 15:00:35 -0500

    div columnas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,20 +2,34 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <header>
-      <nav>
-        <Link href="/">
-          <img src='/images/logo.png' />
-        </Link>
-        <Link href="/about">About</Link>
-        <Link href="/clothing">Clothing</Link>
-        <Link href="/accesories">Accesories</Link>
-        <Link href="/services">Services </Link>
-        <Link href="/login">Log in</Link>
-        <Link href="/signup">
-          <button>Sign Up</button>
-        </Link>
+    <header className='cabecera'>
+      <nav className='menu'>
+        <div className='columna1'>
+          <Link href="/">
+            <img src='/images/logo.png' className='logo' />
+          </Link>
+        </div>
+
+        <div className='columna2'>
+          <Link href="/about">About</Link>
+          <Link href="/clothing">Clothing</Link>
+          <Link href="/accesories">Accesories</Link>
+          <Link href="/services">Services </Link>
+        </div>
+
+        <div className='columna3'>
+          <Link href="/login">Log in</Link>
+          <Link href="/signup">
+            <button className='boton1'>Sign Up</button>
+          </Link>
+
+        </div>
+
+
+
       </nav>
+
+
     </header>
   );
 }
