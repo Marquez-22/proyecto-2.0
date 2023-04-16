af0cb4149d7ab0a66dfcac58d8c87f14a755a109
fatima <marquezlinares19@gmail.com>
2023-04-15 23:38:22 -0500

    head con imagen y boton

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,10 +16,19 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     </Head>
     <header>
       <nav>
+       
+        <Link href="/">
+          <img src='/images/logo.png' />
+        </Link>
         <Link href="/about">About</Link>
         <Link href="/clothing">Clothing</Link>
         <Link href="/accesories">Accesories</Link>
-        <Link href="/services">Services </Link> 
+        <Link href="/services">Services </Link>
+        <Link href="/login">Log in</Link>
+        <Link href="/signup">
+          <button>Sign Up</button>
+        </Link>
+
       </nav>
     </header>
     {children}
